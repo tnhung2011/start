029008c6d54a54120cc88b75f5edca4952cb16e6
IAmOlive <nhathungtran2011@gmail.com>
2021-12-23 18:59:58 +0700

    Initial commit

---
+++ start.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# author: tnhung2011
+# email: nhathungtran2011@gmail.com
+
+from webbrowser import open
+from os import startfile
+from os.path import isfile
+from os.path import exists
+
+
+def start(path, raiseError=False):
+    __920834029384084 = None
+    
+    if exists(path):
+        if isfile():
+            startfile(path)
+        else:
+            open(path)
+            
+        if raiseError is False:
+            return True
+    else:
+        __920834029384084 = open(path)
+        if __920834029384084 is False:
+            if raiseError is True:
+                raise TypeError("FileNotExist")
+            else:
+                return False
+
+def exists(path):
+    if exists(path):
+        return True
+    else:
+        return False
