9a14a8bc2ae6adfe6ea41845f3430c15171a7f33
IAmOlive <nhathungtran2011@gmail.com>
2022-01-04 13:33:53 +0700

    1.0.0-alpha

---
+++ start/__version__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+#   ******** **********     **     *******   **********
+#  **////// /////**///     ****   /**////** /////**/// 
+# /**           /**       **//**  /**   /**     /**    
+# /*********    /**      **  //** /*******      /**    
+# ////////**    /**     **********/**///**      /**    
+#        /**    /**    /**//////**/**  //**     /**    
+#  ********     /**    /**     /**/**   //**    /**    
+# ////////      //     //      // //     //     //   
+
+__title__ = 'start'
+__description__ = 'Open a folder, file or URL like the Run program in Windows with Python.'
+__url__ = 'http://tnhung2011.github.io/start'
+__version__ = '1.0.0-alpha'
+#__build__ = 0x021402
+__author__ = 'Nhat Hung Tran'
+__author_email__ = 'nhathungtran2011@gmail.com'
+__license__ = 'Apache 2.0'
+__copyright__ = 'Copyright (C) 2021-present Nhat Hung Tran'
+__cake__ = u'\u2728 \U0001f370 \u2728'
